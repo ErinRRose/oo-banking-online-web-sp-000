da9b9eada01c73a68a396ed3e55eb8d5cb67db95
Erin  Rose <errose10@gmail.com>
2020-01-30 17:28:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ attr_accessor :status
     # transfer amount
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
+      then
       status == "complete"
     # transfer change status to complete
     #otherwise state check account balance
