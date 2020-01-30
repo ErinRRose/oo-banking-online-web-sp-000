8991a1587c520fbcd757a9a3dcd2eecfa5002cdd
Erin  Rose <errose10@gmail.com>
2020-01-30 17:36:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,4 +28,12 @@ attr_accessor :status
       "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if status == "complete"
+          sender.balance = sender.balance + amount
+          receiver.balance = receiver.balance - amount
+          self.status = "reversed"
+    end
+  end
 end
